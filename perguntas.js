33f40a57d3f039e767b47baab31eaaa21adc1b1e
vitorhugowolfart <vitor.wolfart@escola.pr.gov.br>
2024-10-02 14:41:51 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,59 @@
+criaCartao(
+    'geografia',
+    'qual a capital da jamaica',
+    'kingston'
+)
+
+criaCartao(
+    'Geografia',
+    'qua a capital do Brasil?',
+    'brasilia'
+)
+
+criaCartao(
+    'historia',
+    'quem descobriu o Brasil?',
+    'pedro alvares cabral'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz ola em Inglês?',
+    'ola em ingles é hello'
+)
+
+criaCartao(
+    'ciencia',
+    'qual o maior planeta do sistema solar',
+    'jupiter'
+)
+
+criaCartao(
+    'arte',
+    'quem pintou a monalisa?',
+    'Leonardo da Vinci'
+)
+
+criaCartao(
+    'historia',
+    'quem foi o primeiro presidente dos EUA?',
+    'george washington'
+)
+
+criaCartao(
+    'conhecimento geral',
+    'qual o idioma mais faldo no mundo?',
+    'mandarin'
+)
+
+criaCartao(
+    'geografia',
+    'qual e o maior oceano do mundo?',
+    'pacifico'
+)
+
+criaCartao(
+    'conhecimento geral',
+    'qual e o pais mais populoso do mundo?',
+    'china'
+)
